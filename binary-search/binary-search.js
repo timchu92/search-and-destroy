d013d9bd637c5188d69fbd3a07f8e8d53019b459
Tim Chu <timchu92@gmail.com>
2019-10-23 15:15:05 -0400

    finished binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,19 @@
-'use strict';
+"use strict";
 
+const mid = arr => Math.floor(arr.length / 2);
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+  if (array.length === 1 && array[0] === target) return true;
+  const midpoint = mid(array);
+  const left = array.slice(0, midpoint);
+  const right = array.slice(midpoint);
+  if (target <= left[left.length - 1]) {
+    return binarySearch(left, target);
+  } else if (target > left[left.length - 1]) {
+    return binarySearch(right, target);
+  } else {
+    return false;
+  }
 };
 
 /*
@@ -14,4 +25,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
